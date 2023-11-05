6a4f7b6da29ff07e5754a103459e7dccf8e6b13d
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-05 14:31:05 +0000

    [add]top画面完成。index画面基礎完成。投稿機能途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
+  
+  def new
+    @list = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+  end 
+  
+  def show
+    @book = book.find(params[:id])
+  end 
+  
+  # showが見れるか確認する
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+  
 end
+
+
