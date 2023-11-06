f993d3f79a8a197e5e55eb5575ca04b2636a4738
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-06 12:08:35 +0000

    [add]投稿機能、編集機能、個別表示機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,21 +3,32 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
-  def new
-    @list = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
   end 
   
   def show
-    @book = book.find(params[:id])
+    
+    @book = Book.find(params[:id])
   end 
   
-  # showが見れるか確認する
+  def edit
+    @book = Book.find(params[:id])
+  end 
+  
+  def update
+   #Listテーブルから探し出してきたidのデータをlistに代入
+    book = Book.find(params[:id])
+    #上で代入したデータにアップデートメソッドを実行
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end 
   
   private
   
