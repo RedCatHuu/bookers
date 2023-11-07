54ee9590a6c48463f9603a7d89095f90e701a608
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-07 13:51:41 +0000

    [add]編集失敗時のエラーメッセージ表示。エラーメッセージにスタイル追加。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    
+    @book = Book.new
   end
-  
-  # def new
-  #   @book = Book.new
-  # end
   
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else 
+      @books = Book.all
+      render :index
+    end 
   end 
   
   def show
@@ -23,16 +26,21 @@ class BooksController < ApplicationController
   end 
   
   def update
-   #Listテーブルから探し出してきたidのデータをlistに代入
     book = Book.find(params[:id])
-    #上で代入したデータにアップデートメソッドを実行
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      render :edit
+    end 
   end 
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to request.referer
   end 
   
